0cd73f521e0d750dcd30f8a3df8fe41894d2c15a
Matthias Jäger <write@matthias-jaeger.net>
2020-09-24 19:23:50 +0200

    cleanup

--- fast-snail-app/src/App.js
+++ fast-snail-app/src/App.js
@@ -3,12 +3,9 @@ import React from 'react';
 import ActionMenu from './components/ActionMenu';
 
 function App() {
-  const actionHandler = (e) => {
-    console.log('ss');
-  }
   return (
     <div className='App'>
-      <button onClick={actionHandler}>Add</button>
+      <button>Add</button>
       <ActionMenu></ActionMenu>
     </div>
   );
